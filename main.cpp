597dbdedda4881773b157eefebbe825d87ad9359
QuangDang <quangmeo0007@gmail.com>
2025-05-24 17:52:55 +0700

    mam

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
     cout << "Hello from Linux C++ in VS Code!" << endl;
-    
+    cout << "This is a simple program C++" << endl;
+    cout << "It is running in a Linux environment." << endl;
+    return 0;
 }
  
